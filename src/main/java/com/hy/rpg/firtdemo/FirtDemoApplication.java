35f57cf4354e863dca6653ff2f6476dda7574440
haoy <YS.haoyang@h3c.com>
2020-09-09 12:35:24 +0800

    project init

---
+++ src/main/java/com/hy/rpg/firtdemo/FirtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hy.rpg.firtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirtDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirtDemoApplication.class, args);
+    }
+
+}
